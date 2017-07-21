8e90e58713421850f2e99419e6d48175d7bde80e
Neil Romana <wazzupnickz@gmail.com>
2017-07-20 18:00:43 -0700

    fix comment lines and variables

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
-var app = require('./server.js');
-var port = 3000;
+const app = require('./server.js');
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, function () {
-  console.log('Listening on port ' + port);
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
 });
--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // extracts the entire body portion of incoming req to be used as req.body
+// extracts the entire body portion of incoming req to be used as req.body
+const bodyParser = require('body-parser');
 const path = require('path');
-
-const PORT = process.env.PORT || 3000;
 
 // Create the Express application
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false })); // modify express to take url that contain any format/type of file
-app.use(bodyParser.json()); // parses the text as JSON and set to req.body
+ // modify express to take url that contain any format/type of file
+app.use(bodyParser.urlencoded({ extended: false }));
+// parses the text as JSON and set to req.body
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public/')));
 
 module.exports = app;
