2bb3c437e4232e3e72bd6be317b176e241fb9cb6
Neil Romana <wazzupnickz@gmail.com>
2017-07-20 17:29:17 -0700

    add explanation comments

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // extracts the entire body portion of incoming req to be used as req.body
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 // Create the Express application
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../../public/')));
+app.use(bodyParser.urlencoded({ extended: false })); // modify express to take url that contain any format/type of file
+app.use(bodyParser.json()); // parses the text as JSON and set to req.body
+app.use(express.static(path.join(__dirname, '../public/')));
 
 module.exports = app;
